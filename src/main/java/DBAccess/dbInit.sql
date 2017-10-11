4e7e02a36d968aa02c919c3ca978fc276fef9003
Nemanja <Nemanja@DESKTOP-7E186J9>
2017-10-11 17:15:24 +0200

    Updated sql tables

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -5,13 +5,23 @@ USE `useradmin`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(90) NOT NULL,
   `password` varchar(45) NOT NULL,
   `role` varchar(20) NOT NULL DEFAULT 'customer',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+	`orderid` int(11) NOT NULL AUTO_INCREMENT,
+    `length` int(45) NOT NULL,
+    `width` int(45) NOT NULL,
+    `height` int(45) NOT NULL,
+    `status` DATETIME,
+    PRIMARY KEY (`orderid`),
+    foreign key (orderid) references user(userid));
 
 LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES 
